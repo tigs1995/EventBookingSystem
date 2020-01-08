0edf0f173f98eb65606a0235014c7fd8ae78d066
Tigs Knowles <tigsye@hotmail.com>
2020-01-08 10:14:26 +0000

    Tomcat added in

--- src/main/java/com/bae/Application.java
+++ src/main/java/com/bae/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application{
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
