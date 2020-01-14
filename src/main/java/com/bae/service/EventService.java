303ae496101a789eb1f7e02a828ec5196b158e5e
Simone Knowles <tigsye@hotmail.com>
2020-01-14 21:35:56 +0000

    Email server changed

--- src/main/java/com/bae/service/EventService.java
+++ src/main/java/com/bae/service/EventService.java
@@ -2,11 +2,17 @@ package com.bae.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityNotFoundException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.bae.persistence.repository.CustomerRepository;
@@ -56,6 +62,43 @@ public class EventService {
 	public List<Event> getAllEvents() {
 		return eventRepo.findAll();
 	}
+	
+	public void sendEmail(String email) {
+		final String username = "toastclubltd@gmail.com";
+        final String password = "magojmftzpoaumkg";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("toastclubltd@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(email));
+            message.setSubject("A testing mail header !!!");
+            message.setText("Dear Mail Crawler,"
+                + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+	}
+	
 
 //	public void sendEmail(String email) {
 //
@@ -89,7 +132,7 @@ public class EventService {
 		this.postcode = eventToAdd.getEventPostcode();
 		this.capacity = eventToAdd.getEventCapacity();
 		this.date = eventToAdd.getEventDate().toString();
-//		sendEmail(this.email);
+		sendEmail(this.email);
 		
 		return eventRepo.save(eventToAdd);
 	}
